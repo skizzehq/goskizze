161c780b27e79a7c98102fa0335c170ab59e738c
Neil Jagdish Patel <njpatel@gmail.com>
2016-01-27 18:35:30 +0000

    Add support for creating domains with properties

--- skizze/domain.go
+++ skizze/domain.go
@@ -10,6 +10,13 @@ type Domain struct {
 	Sketches []*Sketch
 }
 
+// DomainProperties details configuration for Sketches in a domain
+type DomainProperties struct {
+	MembershipProperties Properties
+	FrequencyProperties  Properties
+	RankingsProperties   Properties
+}
+
 func newDomainFromRaw(d *pb.Domain) *Domain {
 	ret := &Domain{}
 	ret.Name = d.GetName()
